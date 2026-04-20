80f3a3d668e9f3b629b6dd8e3046e7a06b894b26
Nandhakumar <nandhakumararunachalam2003@gmail.com>
2026-04-20 12:46:52 +0530

    nav-modified

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -13,6 +13,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @stack('styles')
 </head>
@@ -20,9 +22,6 @@
     <div class="app-layout">
         @include('partials.app-nav')
         <div class="app-content-wrap">
-            <header class="app-topbar">
-                <h1 class="app-topbar__title">{{ $title }}</h1>
-            </header>
             <main class="app-main">
                 {{ $slot }}
             </main>
